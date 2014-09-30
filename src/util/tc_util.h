7ce9f98a4d14645dac08f4628745a4c131ea4e40
wangbin579 <wangbin579@gmail.com>
2014-09-30 10:59:11 +0800

    Remove unused code

--- src/util/tc_util.h
+++ src/util/tc_util.h
@@ -3,23 +3,7 @@
 
 #include <xcopy.h>
 
-
-#define TCP_HDR_LEN(tcph) (tcph->doff << 2)
-#define IP_HDR_LEN(iph) (iph->ihl << 2)
-#define EXTRACT_32BITS(p)   ((uint32_t)ntohl(*(uint32_t *)(p)))
-
-#define TCP_PAYLOAD_LENGTH(iph, tcph) \
-        (ntohs(iph->tot_len) - IP_HDR_LEN(iph) - TCP_HDR_LEN(tcph))
-
-#if (TC_UDP)
-#define CHECKSUM_CARRY(x) \
-        (x = (x >> 16) + (x & 0xffff), (~(x + (x >> 16)) & 0xffff))
-#endif
-
-unsigned short csum (unsigned short *pack, int len);
-unsigned short tcpcsum(unsigned char *iphdr, unsigned short *pack, int len);
 int retrieve_devices(char *raw_device, devices_t *devices);
-
 int get_l2_len(const unsigned char *, const int);
 unsigned char *get_ip_data(pcap_t *, unsigned char *, const int, int *);
 
